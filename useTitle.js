76e1d3c7f5532b0469797775e11939d992577ecb
ParkDajeong <sobeast980@gmail.com>
2020-09-08 10:42:53 +0900

    useEffect 활용한 custom hooks

---
+++ useTitle.js
@@ -0,0 +1,14 @@
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+
+  useEffect(updateTitle, [title]);
+
+  return setTitle;
+};
+
+// const titleUpdater = useTitle("Loading...");
+// setTimeout(() => titleUpdater("Home"), 500);
